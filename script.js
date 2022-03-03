e3293d8be757be255d882f57ffe02ec04f3dc606
Giorgio Sangiorgi <giorgiosangiorgi@Giorgios-MacBook-Pro.local>
2022-03-03 15:27:48 +0100

    creation ui and var in script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// variabili
+
+var primo = document.querySelector("#primo");
+var secondo = document.querySelector("#secondo");
+var terzo = document.querySelector("#terzo");
+var quarto = document.querySelector("#quarto");
+var somma = document.querySelector("#somma");
+somma.addEventListener("click",sommavalori);
+var sottrazione = document.querySelector("#sottrazione");
+sottrazione.addEventListener("click",sottraivalori);
+var prodotto = document.querySelector("#prodotto");
+somma.addEventListener("click",moltiplicavalori);
+var divisione = document.querySelector("#divisione");
+divisione.addEventListener("click",dividivalori);
+var risultato = document.querySelector("#risultato");
+var log = document.querySelector("#log")
+
